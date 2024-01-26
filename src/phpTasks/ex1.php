5d628e2c60745c67fb0507e4ab44c328bef94b5f
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-01-26 11:54:59 +0200

    create .php files

---
+++ src/phpTasks/ex1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1: Getting Started with PHP - Chi Nguyen</title>
+</head>
+<body>
+    <h3>3.1 Write PHP code to output the following message:
+        Hello world! My name is "David"
+    </h3>
+    <?php
+        // use backslash to escape special characters like ""
+        echo "Hello world! My name is \"David\"";
+    ?>
+
+    <h3>3.2 Create a PHP variable named $title and assign it the value "PHP is interesting."
+        Then, use this variable as the content within an <h4> (heading 4) element.
+    </h3>
+
+</body>
+</html>
