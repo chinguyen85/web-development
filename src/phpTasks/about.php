91270cf88eda0f8878e38b5a5cd2dacd30b85641
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-01-30 17:40:28 +0200

    Latest update

--- src/phpTasks/about.php
+++ src/phpTasks/about.php
@@ -1,11 +1,27 @@
 <?php 
-    $title = "About Us";
+    $title = "About - Chi Nguyen";
     $md = "This is the about page of my website";
 ?>
 <?php include 'header.php'; ?>
-<h1>About Us</h1>
-<?php
-    echo "Our company is the best!";
-?>
-<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate eligendi cumque eum laudantium saepe voluptatum fugit natus suscipit? Dolor, qui eius. Dolor eos quos suscipit voluptates aspernatur. Harum, voluptas recusandae.</p>
+
+<div class="row body-page">
+    <div class="left-col col-md-6">
+        <h1>About Me</h1>
+        <?php
+            echo "<h4><i>Aim to be a full-stack developer!</i></h4>";
+        ?>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate eligendi cumque eum laudantium saepe voluptatum fugit natus suscipit? Dolor, qui eius. Dolor eos quos suscipit voluptates aspernatur. Harum, voluptas recusandae.</p>
+        <p>Atque, quos expedita modi omnis repellat deleniti minima impedit perspiciatis sit ipsum esse corrupti ipsa facilis asperiores totam mollitia. Distinctio, rerum accusantium!</p>
+        <p>Voluptas cum qui saepe aliquid quas perferendis minus temporibus! Amet magnam totam ratione blanditiis omnis dignissimos, saepe molestias necessitatibus, aliquid architecto voluptatum.</p>
+        <p>Totam ab necessitatibus dolorum fugit quo at odio velit ipsam, porro molestiae ducimus repellendus quis fuga, provident, atque libero nulla officia voluptatibus?</p>
+        <p>Quo obcaecati sint nemo velit numquam omnis quidem distinctio laborum beatae ipsa iste expedita quam, itaque veritatis delectus minima incidunt! Quidem, culpa?</p>
+        <p>Tempore voluptatum praesentium non repudiandae dolor animi quasi voluptatem provident reiciendis blanditiis aliquam sunt asperiores consequuntur dignissimos quia labore assumenda, ut molestias.</p>
+        <p>Voluptate hic modi molestias aperiam, maiores in odit reiciendis libero quos dignissimos vero voluptatem quaerat reprehenderit adipisci animi doloribus error, magnam facere.</p>
+    </div>
+
+    <div class="right-col col-md-6">
+        <img src="https://getbootstrap.com/docs/5.3/assets/img/bootstrap-themes@2x.png" alt="getBootstrap">
+    </div>
+</div><!-- body-page end -->
+
 <?php include 'footer.php'; ?>
--- src/phpTasks/contact.php
+++ src/phpTasks/contact.php
@@ -1,11 +1,27 @@
 <?php 
-    $title = "Contact Us";
+    $title = "Contact - Chi Nguyen";
     $md = "This is the contact page of my website";
 ?>
-
 <?php include 'header.php'; ?>
-<h1>Contact Us</h1>
-<p> 123 Vahdelande 4, 13300 Hameenlinna, FI </p>
-<p> Tel: +358 123 456 789 </p>
-<p> Email: info@hamk.fi </p>
+
+<div class="row body-page">
+    <div class="left-col col-md-6">
+        <h1>Send Your Request</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate eligendi cumque eum laudantium saepe voluptatum fugit natus suscipit? Dolor, qui eius. Dolor eos quos suscipit voluptates aspernatur. Harum, voluptas recusandae.</p>
+        <p>Atque, quos expedita modi omnis repellat deleniti minima impedit perspiciatis sit ipsum esse corrupti ipsa facilis asperiores totam mollitia. Distinctio, rerum accusantium!</p>
+        <p>Voluptas cum qui saepe aliquid quas perferendis minus temporibus! Amet magnam totam ratione blanditiis omnis dignissimos, saepe molestias necessitatibus, aliquid architecto voluptatum.</p>
+        <p>Totam ab necessitatibus dolorum fugit quo at odio velit ipsam, porro molestiae ducimus repellendus quis fuga, provident, atque libero nulla officia voluptatibus?</p>
+        <p>Quo obcaecati sint nemo velit numquam omnis quidem distinctio laborum beatae ipsa iste expedita quam, itaque veritatis delectus minima incidunt! Quidem, culpa?</p>
+
+        <br><h4>Contact Info</h4>
+        <p> 123 Vahdelande 4, 13300 Hameenlinna, FI </p>
+        <p> Tel: +358 123 456 789 </p>
+        <p> Email: info@hamk.fi </p>
+    </div>
+
+    <div class="right-col col-md-6">
+        <img src="https://getbootstrap.com/docs/5.3/assets/img/bootstrap-themes@2x.png" alt="getBootstrap">
+    </div>
+</div><!-- body-page end -->
+
 <?php include 'footer.php'; ?>
