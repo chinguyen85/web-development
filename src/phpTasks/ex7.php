bf2d617b13aeebd7c8da02e58ac840d215be85b8
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-02-05 20:18:21 +0200

    Edit filename of processs.php

--- src/phpTasks/ex7.php
+++ src/phpTasks/ex7.php
@@ -1,13 +1,19 @@
-<h2>Input Your Information Below:</h2>
+<?php 
+    $title = "Exercise 7 - Chi Nguyen";
+    $md = "PHP exercise: CRUD App";
+?>
+<?php include 'header.php'; ?>
+
+<h2>Input Your Information Below</h2>
 <form name="form1" method="post" action="ex7-process.php">
     <div class="form-group">
         <div class="row">
             <div class="col">
-                <label for="fname">First Name:</label>
+                <label for="fname">First Name</label>
                 <input type="text" class="form-control" id="fname" placeholder="Enter your first name" name="fname" required>
             </div>
             <div class="col">
-                <label for="lname">Last Name:</label>
+                <label for="lname">Last Name</label>
                 <input type="text" class="form-control" id="lname" placeholder="Enter your last name" name="lname" required>
             </div>
         </div>
@@ -15,16 +21,17 @@
     <div class="form-group">
         <div class="row">
             <div class="col">
-                <label for="city">City:</label>
+                <label for="city">City</label>
                 <input type="text" class="form-control" id="city" placeholder="Enter your city" name="city" required>
             </div>
             <div class="col">
-                <label for="groupid">Group ID:</label>
+                <label for="groupid">Group ID</label>
                 <select class="form-control" id="groupid" name="groupid">
                     <option value="BBCAP19">BBCAP19</option>
                     <option value="BBCAP20">BBCAP20</option>
                     <option value="BBCAP21">BBCAP21</option>
                     <option value="BBCAP22">BBCAP22</option>
+                    <option value="BBCAP22">BBCAP23</option>
                     <option value="Others">Others</option>
                 </select>
             </div>
@@ -32,3 +39,5 @@
     </div>
     <button type="submit" class="btn btn-primary" name="submit">Submit</button>
 </form>
+
+<?php include 'footer.php'; ?>
