66d0fb6b2a4c2d64cb72e97b00c4973c92cd1a64
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-02-08 10:04:48 +0200

    add Javasript code to about.php

--- src/phpTasks/about.php
+++ src/phpTasks/about.php
@@ -8,6 +8,14 @@
 <?php
     echo "<h4><i>Aim to be a full-stack developer!</i></h4>";
 ?>
+<script> // Javascript code
+    document.write("I am a student at HAMK, studying Computer Applications.");
+</script>
+
+<noscript>
+    <p>Please enable JavaScript to see the content!</p>
+</noscript>
+
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate eligendi cumque eum laudantium saepe voluptatum fugit natus suscipit? Dolor, qui eius. Dolor eos quos suscipit voluptates aspernatur. Harum, voluptas recusandae.</p>
 <p>Atque, quos expedita modi omnis repellat deleniti minima impedit perspiciatis sit ipsum esse corrupti ipsa facilis asperiores totam mollitia. Distinctio, rerum accusantium!</p>
 <p>Voluptas cum qui saepe aliquid quas perferendis minus temporibus! Amet magnam totam ratione blanditiis omnis dignissimos, saepe molestias necessitatibus, aliquid architecto voluptatum.</p>
