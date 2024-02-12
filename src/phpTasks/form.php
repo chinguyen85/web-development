9caf4fad1576a81cc062dca0602003cf9519b7b6
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2024-02-12 11:58:58 +0200

    Correct form.php

--- src/phpTasks/form.php
+++ src/phpTasks/form.php
@@ -1,11 +1,11 @@
 <?php
 $title = "Form Validation - Chi Nguyen";
-include_once 'header.php';
+include_once "header.php";
 ?>
 
 <h2>Sign Up</h2>
 <p><i>Please fill in below information to register!</i></p>
-<form name="sign-up" action="form.php" method="post">
+<form name="sign-up" action="" method="post">
     <div class="form-group">
         <label for="name">Name</label>
         <input type="text" class="form-control" id="name" name="name" required minlength="3" maxlength="30">
@@ -40,7 +40,7 @@ include_once 'header.php';
         const name = document.getElementById("name").value;
         const nameError = document.getElementById("nameError");
         if (name.length < 3 || name.length > 30) {
-            nameError.innerHTML("Name must be at least 3 characters and less than 30 characters");
+            nameError.innerHTML = "Name must be at least 3 characters and less than 30 characters";
             return false;
         }
         else {
@@ -51,51 +51,51 @@ include_once 'header.php';
 
     // function to validate email
     function validateEmail() {
-        const email = document.getElementById('email').value;
-        const emailError = document.getElementById('emailError');
-        if (email === '' || email.includes('@') === -1) {
-            nameError.innerHTML('Email must contain @');
+        const email = document.getElementById("email").value;
+        const emailError = document.getElementById("emailError");
+        if (email === "" || email.includes("@") === -1) {
+            nameError.innerHTML = "Email must contain @";
             return false;
         }
         else {
-            emailError.innerHTML = '';
+            emailError.innerHTML = "";
             return true;
         }
     }
 
     // function to validate password
     function validatePassword() {
-        const password = document.getElementById('password').value;
-        const passwordError = document.getElementById('passwordError');
+        const password = document.getElementById("password").value;
+        const passwordError = document.getElementById("passwordError");
         if (password.length < 6) {
-            passwordError.innerHTML('Password must be at least 6 characters.');
+            passwordError.innerHTML("Password must be at least 6 characters.");
             return false;
         }
         else {
-            passwordError.innerHTML = '';
+            passwordError.innerHTML = "";
             return true;
         }
     }
     
     // function to validate age
     function validateAge() {
-        const age = document.getElementById('age').value;
-        const ageError = document.getElementById('ageError');
+        const age = document.getElementById("age").value;
+        const ageError = document.getElementById("ageError");
         if (age < 18) {
-            ageError.innerHTML('You must be at least 18 years old');
+            ageError.innerHTML("You must be at least 18 years old");
             return false;
         }
         else {
-            ageError.innerHTML = '';
+            ageError.innerHTML = "";
             return true;
         }
     }
 
     // event listener for real time validation
-    document.getElementById('name').addEventListener('input', validateName);
-    document.getElementById('email').addEventListener('input', validateEmail);
-    document.getElementById('password').addEventListener('input', validatePassword);
-    document.getElementById('age').addEventListener('input', validateAge);
+    document.getElementById("name").addEventListener("input", validateName);
+    document.getElementById("email").addEventListener("input", validateEmail);
+    document.getElementById("password").addEventListener("input", validatePassword);
+    document.getElementById("age").addEventListener("input", validateAge);
 </script>
 
-<?php include_once 'footer.php'; ?>
+<?php include_once "footer.php"; ?>
