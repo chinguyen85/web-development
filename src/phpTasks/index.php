732c2834a0e101ba01d763c0eb46b3042e5fa34d
Dinh Khanh Chi Nguyen <khanhchi.fe@gmail.com>
2025-05-06 20:08:16 +0300

    Update respository with additional files

---
+++ src/phpTasks/index.php
@@ -0,0 +1,15 @@
+<?php 
+    $title = "Home - Chi Nguyen";
+    $md = "PHP exercises";
+?>
+
+<?php include_once 'header.php'; ?>
+
+<h1>A HAMK Junior Student In Computer Application</h1>
+<div style="margin-top:16px"></div>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur animi minus accusantium earum et itaque obcaecati nihil iusto exercitationem, natus eaque voluptatem. Officia aperiam dolorem praesentium iste quia quibusdam exercitationem.</p>
+<p>Saepe nihil exercitationem nesciunt inventore unde nostrum corrupti ratione fugit ut ullam. Iste a consectetur eligendi, eum numquam praesentium iusto adipisci reprehenderit?</p>
+<a class="btn btn-primary" href="ex1.php" role="button">View more</a>
+<br>
+
+<?php include_once 'footer.php'; ?>
